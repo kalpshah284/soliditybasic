3e9ed7f896ac8584a06e94f0c7dcbe2af6af1c9a
kalpshah6161 <kalpshah284@gmail.com>
2023-10-21 17:05:13 +0530

    Deleted repetitive files

--- encode.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract EncodeExample {
-    function encodeExample(uint256 value, string memory text) public pure returns (bytes memory) {
-        bytes memory encodedData = abi.encode(value, text);
-        return encodedData;
-    }
-}
